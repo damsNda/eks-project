d62bcf9b6d41cd34e7ac007dfec8e51c9da920e2
damsNda <damien.nda@gmail.com>
2020-12-23 19:24:12 +0100

    angular app

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const httpPort=process.env.NODE_PORT || 3000;
 
 app.get('/personnes', (req, res) => {
     let personnes= [{"id":1, firstName: "Damien", lastName: "N DA"},{"id":2, firstName: "Jeanne", lastName: "N DA"}];
     res.json(personnes);
 });
 
-app.listen(5000, (err) => {
-    console.log('Listening');
+app.listen(httpPort, (err) => {
+    console.log(`Express Server is up ..Listening on port ${httpPort}`);
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  personnes:Personn[];
+
+  constructor(){
+    this.init();
+    this.personnes=[];
+  }
+
+  public async init(): Promise<void>{
+    console.log("Iniiiiiiiiiiiit");
+    await axios.get<Personn[]>("/api/personnes").then((response) => {
+      this.personnes=response.data;
+    });
+    console.log("End Iniiiiiiiiiiiit " + JSON.stringify(this.personnes));
+  }
+
+}
+
+export class Personn{
+ 
+  constructor(public  id:string, public  firstName:string,public  lastName:string){
+
+  }
 }
