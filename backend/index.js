5c0e99dfc7f6a0e335fbdb7778434f865de3b4a5
damsNda <damien.nda@gmail.com>
2020-12-24 00:43:29 +0100

    toto2

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const httpPort=process.env.NODE_PORT || 3000;
 
 app.get('/personnes', (req, res) => {
-    let personnes= [{"id":1, firstName: "Vador", lastName: "Darth"},{"id":2, firstName: "Zinedine", lastName: "Zidane"}];
+    let personnes= [{"id":1, firstName: "Vador", lastName: "Darth"},{"id":2, firstName: "Zinedine", lastName: "Zidane"},{"id":3, firstName: "Diego Armando", lastName: "Maradona"}];
     res.json(personnes);
 });
 
